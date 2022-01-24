577f31e6c80103099dbf1557bb7ffe1e1b248903
Greenphx9 <lochlanly@gmail.com>
2022-01-24 11:29:15 +1100

    bananza

---
+++ bananza/README.md
@@ -0,0 +1,4 @@
+## Bananza
+Replicates the bananza mode from Battles 1 (double starting cash, eco, etc).
+
+For income towers, only the banana farm has been changed.
---
+++ bananza/start.lua
@@ -0,0 +1,6 @@
+mod_metadata("name", "Bananza By Greenphx")
+
+jet_binary_replace("game_data/game_project/assets/data/bloon_sets.bloonset", "bloon_sets.bloonset")
+jet_binary_replace("game_data/game_project/assets/data/default.gamerules", "default.gamerules")
+jet_binary_replace("game_data/game_project/assets/towers/banana_farm/data/banana_farm.tower_blueprint", "banana_farm.tower_blueprint")
+jet_binary_replace("game_data/game_project/assets/data/default_private.gamerules", "default_private.gamerules")
